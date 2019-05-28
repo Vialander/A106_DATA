2d8a0db4a17a255e8635835fa6b196acc775d929
Vialander <602699500@qq.com>
2019-05-28 14:10:50 +0800

    试用版本，设置输入路径和输出路径

--- rss_write_foruse.m
+++ rss_write_foruse.m
@@ -1,15 +1,17 @@
-str = 'F';
+input_index = 'indoor';
+output_index = 'Sample_indoor';
+str = 'H';
 
 for num = 1:15
     
     name_num = num2str(num);
     name_string = [str num2str(num) '.xls'];
-    name = fullfile('Sample_indoor',name_string);
+    name = fullfile(output_index, name_string);
     
-    aa1 = fullfile('A106','indoor',str,name_num,'AP_A.dat');
-    bb1 = fullfile('A106','indoor',str,name_num,'AP_B.dat');
-    cc1 = fullfile('A106','indoor',str,name_num,'AP_C.dat');
-    dd1 = fullfile('A106','indoor',str,name_num,'AP_D.dat');
+    aa1 = fullfile('A106',input_index,str,name_num,'AP_A.dat');
+    bb1 = fullfile('A106',input_index,str,name_num,'AP_B.dat');
+    cc1 = fullfile('A106',input_index,str,name_num,'AP_C.dat');
+    dd1 = fullfile('A106',input_index,str,name_num,'AP_D.dat');
     
     A1 = read_bf_file(aa1);
     B1 = read_bf_file(bb1);
